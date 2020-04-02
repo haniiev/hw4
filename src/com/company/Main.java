ed8acd52d549cb09ffbaea5e09afa8b7930aa8aa
Anton <39731038+haniiev@users.noreply.github.com>
2020-04-02 19:55:36 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,23 +1,36 @@
 package com.company;
 
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        empl[] emplsArr = new empl[5];
-        emplsArr[0] = new empl("Ivan", 1, 1000);
-        emplsArr[1] = new empl("Vasya", 1, 2000);
-        emplsArr[2] = new empl("Vitya", 2, 3000);
-        emplsArr[3] = new empl("Kolya", 2, 4000);
-        emplsArr[4] = new empl("Akakiy", 3, 5000);
+    public static void main(String[] args) {
+	int[] arr = {1,2,3,-4,5,6,-7,8,-9,10};
+	   int max = arr[0];
+       int imax = 0;
+       int i = 0;
+       while (i < arr.length) {
+           if (arr[i] > max) {
+               max = arr[i];
+               imax = i;
+           }
+         i++;
+       }
+        System.out.println(max);
 
-        int dep = new Scanner(System.in).nextInt();
-        for (int i = 0; i < emplsArr.length; i++) {
-            if (emplsArr[i].getDepartment() == dep)
-                System.out.println(emplsArr[i]);
-        }
+       int count = 0;
+       for(int element : arr) {
+           if(element > 0){
+               count++;
+           }
+       }
+        System.out.println(count);
+
+       int sum = 0;
+       for(int element : arr) {
+           if(element < 0){
+               sum += element;
+           }
+       }
+        System.out.println(sum);
+
     }
 }
-
